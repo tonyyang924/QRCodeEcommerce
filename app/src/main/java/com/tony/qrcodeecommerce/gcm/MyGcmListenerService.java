d03cd43344ece46322a62b171e3bfb6c0ab8a915
Zoro <superzoro168@gmail.com>
2015-10-03 17:30:45 +0800

    修改gcm推播訊息與修正，並且放入demo資料。

--- app/src/main/java/com/tony/qrcodeecommerce/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/tony/qrcodeecommerce/gcm/MyGcmListenerService.java
@@ -11,7 +11,6 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
-import com.tony.qrcodeecommerce.MainActivity;
 import com.tony.qrcodeecommerce.R;
 import com.tony.qrcodeecommerce.UserOrderDetails;
 import com.tony.qrcodeecommerce.utils.AppSP;
@@ -56,17 +55,19 @@ public class MyGcmListenerService extends GcmListenerService {
      */
     private void sendNotification(String message) {
         Log.i(TAG,"message:"+message);
-        String[] strArr = message.split(":");
+        //標題、內容、OID
+        String[] strArr = message.split(";");
         Intent intent;
         AppSP appSP = new AppSP(getApplicationContext());
-        if(appSP.getIsAdmin()) {
-            intent = new Intent(this, UserOrderDetails.class);
-            Bundle bundle = new Bundle();
-            bundle.putString("oid",strArr[1]);
-            intent.putExtras(bundle);
-        } else {
-            intent = new Intent(this, MainActivity.class);
-        }
+//        if(appSP.getIsAdmin()) {
+        intent = new Intent(this, UserOrderDetails.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("oid",strArr[2]);
+        intent.putExtras(bundle);
+//        }
+//        else {
+//            intent = new Intent(this, MainActivity.class);
+//        }
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
@@ -74,8 +75,8 @@ public class MyGcmListenerService extends GcmListenerService {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.qrcodereader)
-                .setContentTitle("訂單訊息")
-                .setContentText(message)
+                .setContentTitle(strArr[0])
+                .setContentText(strArr[1])
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
